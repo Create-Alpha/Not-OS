3c4fe9753f916955d00a8cd0c222d74e925375a2
kklsuid <2441389288@qq.com>
2023-01-12 23:42:41 +0800

    v0.8.2

--- README.md
+++ README.md
@@ -1,46 +1,11 @@
 # Not OS
 
-Tips: this version is a simple version for using IntelliCode's people.
+Tips: this version is a simple version for using IC / LSP's people.
+
+(Because the original version is not supported by them)
 
 If you want to download a release, go to main branch.
 
-[![Last updated](https://img.shields.io/badge/dynamic/json?color=red&label=Last%20Updated&query=updated_at&url=https%3A%2F%2Fapi.github.com%2Frepos%2FCreate-Alpha%2FNot-OS)](https://github.com/Create-Alpha/Not-OS/releases/latest)
+------------------------
 
-A simple system using asm &amp; c++
-
-## How to install (VirtualBox)
-
-First, install **VirtualBox**.
-
-Next, get **.iso file** (Releases or build yourself)
-
-Create a new virtual machine in vbox, change its type to **Other/Unknown**.
-
-Then rename it to **"NotOS"**. Set the **dvd storage** to the .iso file.
-
-Run it. If it works, you may see something like these:
-
-```text
-NotOS v0.7.0-alpha
-Hello, World!   From NoFun
-__________________________
-(c) 2023 Create Alpha Tech
-   
-Initializing Hardware, Stage 1.. 2.. 3
-```
-
-## How to build
-
-Need: **Linux with git, g++, make, binutils, grub2, and xorriso**.
-
-Enter this to install them: `sudo apt-get install g++ make binutils xorriso grub2-common git`
-
-Then clone project: `git clone git@github.com:kklsuid/NotOS-Real.git`
-
-If you're ready, change to project's dir, and enter this to build a .iso file.
-
-`make mykernel.iso`
-
-Or you want a .bin file, enter
-
-`make mykernel.bin`
+How to build & run: Check main branch's README.
--- hardwarecommunication/pci.h
+++ hardwarecommunication/pci.h
@@ -11,24 +11,40 @@ namespace NotOS
 {
     namespace hardwarecommunication
     {
+        enum BaseAddressRegisterType
+        {
+            MemoryMapping = 0,
+            InputOutput = 1
+        };
+
+        class BaseAddressRegister
+        {
+        public:
+            bool prefetchable;
+            NotOS::common::uint8_t* address;
+            NotOS::common::uint32_t size;
+            BaseAddressRegisterType type;
+
+        };
+
         class PCIDeviceDescriptor
         {
         public:
-            common::uint32_t portBase;
-            common::uint32_t interrupt;
+            NotOS::common::uint32_t portBase;
+            NotOS::common::uint32_t interrupt;
 
-            common::uint16_t bus;
-            common::uint16_t device;
-            common::uint16_t function;
+            NotOS::common::uint16_t bus;
+            NotOS::common::uint16_t device;
+            NotOS::common::uint16_t function;
 
-            common::uint16_t vendor_id;
-            common::uint16_t device_id;
+            NotOS::common::uint16_t vendor_id;
+            NotOS::common::uint16_t device_id;
 
-            common::uint8_t class_id;
-            common::uint8_t subclass_id;
-            common::uint8_t interface_id;
+            NotOS::common::uint8_t class_id;
+            NotOS::common::uint8_t subclass_id;
+            NotOS::common::uint8_t interface_id;
 
-            common::uint8_t revision;
+            NotOS::common::uint8_t revision;
 
             PCIDeviceDescriptor();
             ~PCIDeviceDescriptor();
@@ -42,12 +58,14 @@ namespace NotOS
             PCIController();
             ~PCIController();
 
-            common::uint32_t Read(common::uint16_t bus, common::uint16_t device, common::uint16_t function, common::uint32_t registeroffset);
-            void Write(common::uint16_t bus, common::uint16_t device, common::uint16_t function, common::uint32_t registeroffset, common::uint32_t value);
-            bool DeviceHasFunctions(common::uint16_t bus, common::uint16_t device);
-            void SelectDrivers(NotOS::drivers::DriverManager* driverManager);
+            NotOS::common::uint32_t Read(NotOS::common::uint16_t bus, NotOS::common::uint16_t device, NotOS::common::uint16_t function, NotOS::common::uint32_t registeroffset);
+            void Write(NotOS::common::uint16_t bus, NotOS::common::uint16_t device, NotOS::common::uint16_t function, NotOS::common::uint32_t registeroffset, NotOS::common::uint32_t value);
+            bool DeviceHasFunctions(NotOS::common::uint16_t bus, NotOS::common::uint16_t device);
+            void SelectDrivers(NotOS::drivers::DriverManager* driverManager, NotOS::hardwarecommunication::InterruptManager* interrupts);
+            NotOS::drivers::Driver* GetDriver(PCIDeviceDescriptor dev, NotOS::hardwarecommunication::InterruptManager* interrupts);
 
-            PCIDeviceDescriptor GetDeviceDescriptor(common::uint16_t bus, common::uint16_t device, common::uint16_t function);
+            PCIDeviceDescriptor GetDeviceDescriptor(NotOS::common::uint16_t bus, NotOS::common::uint16_t device, NotOS::common::uint16_t function);
+            BaseAddressRegister GetBaseAddressRegister(NotOS::common::uint16_t bus, NotOS::common::uint16_t device, NotOS::common::uint16_t function, NotOS::common::uint16_t bar);
         };
     }
 }
